3570af703d3fffe23ff13ac2d1e8dda9b3d0af63
SAEHIM <sh.kwon@vhows.com>
2021-10-04 22:04:55 +0900

    엔티티 설계시 주의점
    -LAZY설정
    -연관관계 편의메서드 추가
    -cascade 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,14 +19,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // cascade => orderItems에 데이터 넣어두고 Order를 저장하면 orderItems도 같이 저장
     private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,4 +34,21 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus; // 주문태 [ORDER, CANCEL]
+
+    // 연관관계 편의 메서드의 위치는 실질적으로 컨트롤 하는쪽에 위치시키는것이 좋음
+    //==연관관계 편의메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
